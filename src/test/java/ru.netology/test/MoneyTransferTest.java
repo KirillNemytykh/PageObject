7457185afb40c1183bed8ece26a93538ccc9f294
Kirill Nemytykh <nemytykhkirill@gmail.com>
2024-03-17 21:05:06 +0700

    first commit

---
+++ src/test/java/ru.netology/test/MoneyTransferTest.java
@@ -0,0 +1,44 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPageV2;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DataHelper.*;
+
+public class MoneyTransferTest {
+
+    DashboardPage dashboardPage;
+
+    @BeforeEach
+    void setup() {
+        Selenide.open("http://localhost:9999");
+        var authInfo = getAuthInfo();
+        var verificationPage = LoginPageV2.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferFromFirstCardToSecond() {
+        var firstCardInfo = DataHelper.getFirstCartInfo();
+        var secondCardInfo = DataHelper.getSecondCartInfo();
+        var firstCardBalance = dashboardPage.getCardBalance(getFirstCartInfo());
+        var secondCardBalance = dashboardPage.getCardBalance(getSecondCartInfo());
+        var amount = generateValidBalance(firstCardBalance);
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+}
+
